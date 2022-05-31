70b22efe269f4fcf816218665c263c746c85c22d
Ramirez Brian <tiziano0705@hotmail.com>
2022-05-30 21:46:27 -0300

    Desafio For

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ for (i = 1; i <= 20; i++){
     nombreApellido =(prompt ('Ingrese nombre y apellido'))
     lista.push(nombreApellido);
 
+    if (i == 20) {
+        alert("Se terminaron los cupos de incripcion");
+    }
+
 }
 console.log(lista);
 
