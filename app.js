a56b8316f50611636e3671634e5e512d99110696
Ramirez Brian <tiziano0705@hotmail.com>
2022-05-30 21:30:14 -0300

    Desafio forç

---
+++ app.js
@@ -0,0 +1,13 @@
+let nombreApellido = "";
+const lista =[""];
+
+for (i = 1; i <= 20; i++){
+    nombreApellido =(prompt ('Ingrese nombre y apellido'))
+    lista.push(nombreApellido);
+
+}
+console.log(lista);
+
+
+
+
