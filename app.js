d656aa27fd98bda13f49b4d2e57effaaa0a93457
Ramirez Brian <tiziano0705@hotmail.com>
2022-06-15 23:45:29 -0300

    desafio arrays

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
-let nombreApellido = "";
-const lista =[""];
+// let nombreApellido = "";
+// const lista =[""];
 
-for (i = 1; i <= 20; i++){
-    nombreApellido =(prompt ('Ingrese nombre y apellido'))
-    lista.push(nombreApellido);
+// for (i = 1; i <= 20; i++){
+//     nombreApellido =(prompt ('Ingrese nombre y apellido'))
+//     lista.push(nombreApellido);
 
-    if (i == 20) {
-        alert("Se terminaron los cupos de incripcion");
-    }
+//     if (i == 20) {
+//         alert("Se terminaron los cupos de incripcion");
+//     }
 
+// }
+// console.log(lista);
+let usuario = ""
+
+const crearUsuarios = [{
+    usuario:"",
+    contrasenia:"",
+    mail:""
+}]
+
+let listaDeUsuarios = []
+let dataUsuarios = new Object()
+
+for(i = 0; i < 5; i++){
+dataUsuarios = {}
+dataUsuarios['usuario'] = prompt ("ingresa usuario")
+dataUsuarios['contrasenia'] = prompt ("ingresa contrasenia")
+dataUsuarios['mail'] = prompt ("ingresa mail")
+listaDeUsuarios.push(dataUsuarios);
 }
-console.log(lista);
-
-
-
-
+console.log(listaDeUsuarios);
